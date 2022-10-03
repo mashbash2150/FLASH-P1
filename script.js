86632afdc6dd0c49295dcb6acbf00577754809f4
Madeline A <madeline.ashley2@gmail.com>
2022-10-03 12:39:57 -0500

    A few sequence play functions added

--- script.js
+++ script.js
@@ -2,11 +2,35 @@
 let randSequence=[]
 let userSequence=[]
 let maxSeqItems=4
+const pink=document.querySelector(".pink")
+const purple=document.querySelector(".purple")
+const yellow=document.querySelector(".yellow")
+const aqua=document.querySelector(".aqua")
+const colorOrder=[pink,purple,yellow,aqua]
 
 //functions
-const levelParams =()=>{
-  
+function levelParams() {
 }
+const playSequence=(seq)=>{
+
+  //have a class that applies visual queues to circle that mimics a flash
+    for(let i=0;i<seq.length;i++){
+      seq[i].style.opacity=1.0
+    }
+    function task(i) {
+      setTimeout(function() {
+      }, 2000 * i);
+    }
+ }
+const mapSequence=(numSeq)=>{
+  let colorSequence=numSeq.map((number)=>{
+    return colorOrder[number-1]
+    return colorSequence;
+  })
+  //return colorSequence
+  playSequence(colorSequence)
+}
+
 
  const generateSequence=(max)=>{
   //generate number between 1 and number of Seq items in current level
@@ -15,16 +39,13 @@ const levelParams =()=>{
       randNumber=Math.floor(Math.random()*max)+1
       randSequence.push(randNumber)
   }
-  console.log(randSequence)
+  mapSequence(randSequence)
  }
 
- const playSequence=()=>{
 
-  //have a class that applies visual queues to circle that mimics a flash
-  // randSequence.forEach((number)=>{
-     
-  // }
- }
+
+
+
  
  const startTimer=()=>{
 
@@ -38,11 +59,23 @@ const levelParams =()=>{
 
  }
 
-generateSequence(maxSeqItems)
+//
 
 
 
 
 
 //event listeners
+
+const level=document.querySelector(".test")
+
+const goEventListener = ()=>{
+  level.addEventListener('click',function(){
+    generateSequence(maxSeqItems)
+  })
+}
+
+goEventListener()
+
+
 
